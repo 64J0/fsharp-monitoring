880d415ad853312fd04bc529135a76f91745bd43
64J0 <vinigaio97@gmail.com>
2023-07-21 19:10:03 -0300

    chore: move the environment variables reading to the Program.fs and update the .net version to 7

--- Fsharp-API/Program.fs
+++ Fsharp-API/Program.fs
@@ -1,8 +1,21 @@
 ﻿open Saturn
 
+open System
+
 open API.Server
+
+let setEnvVarDefaultValue (defaultValue: string) (readEnvVar: string) =
+    match readEnvVar with
+    | null -> defaultValue
+    | _ -> readEnvVar
+
+let HOST: string =
+    Environment.GetEnvironmentVariable "HOST" |> setEnvVarDefaultValue "localhost"
+
+let PORT: string =
+    Environment.GetEnvironmentVariable "PORT" |> setEnvVarDefaultValue "8085"
 
 [<EntryPoint>]
 let main (args: string[]) =
-    run serverConfig
+    run (serverConfig (HOST) (PORT))
     0
--- Fsharp-API/src/Server.fs
+++ Fsharp-API/src/Server.fs
@@ -1,26 +1,12 @@
 module API.Server
 
-// https://saturnframework.org/reference/Saturn/saturn-application-applicationbuilder.html
-
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Logging
 open Microsoft.Extensions.DependencyInjection
 open Saturn
 open Prometheus
-open System
 
 open API.Router
-
-let private setEnvVarDefaultValue (defaultValue: string) (readEnvVar: string) =
-    match readEnvVar with
-    | null -> defaultValue
-    | _ -> readEnvVar
-
-let private HOST: string =
-    Environment.GetEnvironmentVariable "HOST" |> setEnvVarDefaultValue "localhost"
-
-let private PORT: string =
-    Environment.GetEnvironmentVariable "PORT" |> setEnvVarDefaultValue "8085"
 
 let private loggingConfig (builder: ILoggingBuilder) =
     builder.SetMinimumLevel LogLevel.Information |> ignore
@@ -28,19 +14,16 @@ let private loggingConfig (builder: ILoggingBuilder) =
 // https://www.compositional-it.com/news-blog/dependency-injection-with-asp-net-and-f/
 let private configureServices (services: IServiceCollection) = services.AddHttpClient()
 
-// https://gist.github.com/pecigonzalo/463ebb7d6f8ed7b8b102f000edb8cf6b#metrics
-// TODO: start the metrics server in a different PORT
-// this way we can segregate the API server from the metrics server
 let private configureApp (app: IApplicationBuilder) = app.UseHttpMetrics().UseMetricServer()
 
-let serverConfig =
+let serverConfig (host: string) (port: string) =
     application {
         logging loggingConfig
         use_antiforgery
         use_gzip
 
         use_router appRouter
-        url (sprintf "http://%s:%s" HOST PORT)
+        url (sprintf "http://%s:%s" host port)
 
         service_config configureServices
         app_config configureApp
