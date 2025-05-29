64fcec9ee09c46b4d4aba0913ed9cae35a7b5d31
64J0 <vinigaio97@gmail.com>
2025-05-29 09:43:32 -0300

    fix (api): return Some ctx for the requestCounter middleware so the server does not crash due to the response status code being changed after the request was already answered

--- fsharp-api/Router/Prometheus.middleware.fs
+++ fsharp-api/Router/Prometheus.middleware.fs
@@ -7,8 +7,6 @@ open Prometheus
 
 open API.MonitoringPrometheus
 
-// TODO debug why this middleware is trying to change the response status code
-// after the request finishes
 let requestCounter: HttpHandler =
     fun (next: HttpFunc) (ctx: HttpContext) ->
         task {
@@ -28,7 +26,28 @@ let requestCounter: HttpHandler =
             requestCounter.WithLabels(endpoint, method, statusCode).Inc()
 
             // Skip the remaining pipeline
-            return None
+            //
+            // WARNING!!!!
+            //
+            // We need to return `Some ctx` instead of `None` here, otherwise, when
+            // reaching the **handleResult** (https://github.com/giraffe-fsharp/Giraffe/blob/master/src/Giraffe/EndpointRouting.fs#L101-L104)
+            // Giraffe will try to set the response status code for a request that
+            // was already finished. This will throw some exception that one can
+            // finds at the server logs.
+            //
+            // Something that I still don't fully understand is why this exception
+            // related to the status code change does not happen for all the cases.
+            // When testing it was only happening when the /api/prediction endpoint
+            // fails (i.e., throws an exception itself).
+            //
+            // To test this error locally, just change this "return Some ctx" to
+            // "return None", start the API and use the /api/prediction endpoint with
+            // an invalid payload, like:
+            //
+            // curl -X POST -H "Accept: application/json" -d '{"id":"1", "crimesPerCapta":0.01}' localhost:8088/api/prediction
+            //
+            // P.S.: Notice that the id is a string instead of an integer ^.
+            return Some ctx
         }
 
 let requestDuration: HttpHandler =
